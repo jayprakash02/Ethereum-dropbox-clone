046e4380a20b9f9ff09d832a40f5b9608c97219f
Jay Prakash <unijay12@gmail.com>
2020-12-28 19:37:24 +0530

    1st migration

--- src/contracts/DStorage.sol
+++ src/contracts/DStorage.sol
@@ -1,18 +1,50 @@
 pragma solidity ^0.5.0;
 
 contract DStorage {
-  // Name
-  // Number of files
-  // Mapping fileId=>Struct 
-
+  string public name='Dstorage';
+  uint public fileCount;
+  mapping(uint => File)public files;
   // Struct
+  struct File{
+    uint fileid;
+    string fileHash;
+    uint fileSize;
+    string fileType;
+    string fileName;
+    string fileDesciption;
+    uint uploadTime;
+    address payable uploader;
+  }
 
-
-  // Event
+  event FileUploaded(
+    uint fileid,
+    string fileHash,
+    uint fileSize,
+    string fileType,
+    string fileName,
+    string fileDesciption,
+    uint uploadTime,
+    address payable uploader
+  );
 
   constructor() public {
   }
+  function uploadFile(string memory _fileHash,uint _fileSize,string memory _fileType,string memory _fileName,string memory _fileDesciption){
+    
+    require(bytes(_fileHash).length >0);
+    require(bytes(_fileType).length >0);
+    require(bytes(_fileDesciption).length >0);
+    require(bytes(_fileName).length >0);
+    require(msg.sender != address(0));
+    require(_fileSize > 0);
 
+    
+    
+    fileCount++;
+    files[fileCount]=File(fileCount,_fileHash,_fileSize,_fileType,_fileName,_fileDesciption,now,msg.sender);
+
+    emit FileUploaded(fileCount,_fileHash,_fileSize,_fileType,_fileName,_fileDesciption,now,msg.sender);
+  }
   // Upload File function
 
     // Make sure the file hash exists
